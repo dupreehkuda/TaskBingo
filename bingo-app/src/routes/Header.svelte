b92176723c8e7c622d764a3d599c873e6de0f8f1
Danila <danyakurach@gmail.com>
2022-12-24 16:01:40 +0300

    changed appearance

--- bingo-app/src/routes/Header.svelte
+++ bingo-app/src/routes/Header.svelte
@@ -9,11 +9,11 @@
 <main>
     <Navbar let:hidden let:toggle color="none">
         <NavBrand href="/">
-            <img
+            <!-- <img
                     src="https://flowbite.com/docs/images/logo.svg"
                     class="mr-3 h-6 sm:h-9"
                     alt="Flowbite Logo"
-            />
+            /> -->
             <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Task Bingo</span>
         </NavBrand>
         <NavHamburger on:click={toggle}/>
